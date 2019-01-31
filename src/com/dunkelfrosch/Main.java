7b3f3d4f0ae04a0c7f19f5df6515f0a68318a085
Patrick Paechnatz <patrick.paechnatz@t-systems-mms.com>
2019-01-31 07:42:21 +0100

    first commit

---
+++ src/com/dunkelfrosch/Main.java
@@ -0,0 +1,18 @@
+package com.dunkelfrosch;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int number = 2147483647;
+
+        int[][] matrix = {
+                {1},
+                {2,3},
+                {4,5,6},
+                {7,8,9,10}};
+
+        // enter your code here ...
+    }
+}
